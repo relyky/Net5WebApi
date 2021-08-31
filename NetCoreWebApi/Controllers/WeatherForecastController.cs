ad04dc70e534a4823bdd5098217dade1e8db6f58
Rely Kao <rely_kao@asiavista.com.tw>
2021-08-31 10:05:02 +0800

    Serilog

--- NetCoreWebApi/Controllers/WeatherForecastController.cs
+++ NetCoreWebApi/Controllers/WeatherForecastController.cs
@@ -26,6 +26,8 @@ namespace NetCoreWebApi.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation(@"WeatherForecastController\Get");
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
--- NetCoreWebApi/Program.cs
+++ NetCoreWebApi/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +14,42 @@ namespace NetCoreWebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Web host start.");
+                CreateHostBuilder(args).Build().Run();
+                Log.Information("Web host exit.");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Web host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        /// <summary>
+        /// used by Serilog to read appsettings.json
+        /// </summary>
+        public static IConfiguration Configuration => new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddJsonFile($"appsettings.{Environment.MachineName}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
     }
 }
